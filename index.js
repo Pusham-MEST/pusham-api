618aaca105cd8fe19ff4c52a5eac8121a7dfb8e7
Adeline Ackun <adelineackun@gmail.com>
2024-07-31 13:38:33 +0000

    first

--- index.js
+++ index.js
@@ -1,12 +1,23 @@
 // Import packages
 import express from "express";
 import mongoose from "mongoose";
+import { checkUserSession } from "./middleware/auth.js";
 
 // create express app
 const app = express();
 
 // Db connection
 await mongoose.connect(process.env.MONGO_URI)
+console.log("Database is connected")
+
+// Middleware
+// To Parse incomming JSON request and put the parsed data in req.body 
+app.use(express.json());
+
+// To custom middlewate to check user session or token
+app.use(checkUserSession);
+
+
 
 // Port-listening connection
 const port = process.env.PORT || 3050;
