308e7cf625f8f6df75070a46a61cc81b3101cdc0
FAMfreda <mbordfreda@gmail.com>
2024-08-07 19:18:59 +0000

    added app to index

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
 import userRouter from "./router/user_router.js";
 import express from "express";
 import mongoose from "mongoose";
+import { getOutagesByNeighbourhood } from "./controllers/outageController.js";
+import outageRouter from "./routes/outages.js";
+import neighbourhoodRouter from "./routes/neighborhood.js";
 
 
 // create express app
@@ -12,6 +15,12 @@ app.use(express.json());
 
 
 app.use(userRouter)
+app.use(neighbourhoodRouter)
+app.use(outageRouter)
+
+
+
+
 // Db connection
 await mongoose.connect(process.env.mongo_url).then(() => {
     console.log('Database is connected');
